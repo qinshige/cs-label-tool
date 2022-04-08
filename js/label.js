565aa4f946be9f4eea86a271c13f919fe408e04a
qinshige.vendor <qinshige.vendor@sensetime.com>
2022-04-08 13:56:27 +0800

    涂抹功能补充

--- js/label.js
+++ js/label.js
@@ -4,7 +4,6 @@ class AiLabel {
 	canvas2;
 	cs2Ctx;
 	cs1Ctx;
-	// --------- 画笔和橡皮擦功能实现 -------------
 	flag = false;  // 记录鼠标按下和抬起事件，标记工具是否开始工作
 	isEraser; // 判断工具是画笔还是橡皮擦
 	lineWidth = 30; // 设置工具粗细
@@ -13,29 +12,62 @@ class AiLabel {
 	setDraw() {
 		this.cs2Ctx.strokeStyle = theColor;
 	}
+	/**
+	 * el // 节点id
+	 * src // 图片路径
+	 * @param {*} config
+	 */
 	initMap(config) {
-		this.main = document.querySelector(config.id);
-		const canvas1 = document.createElement('canvas');
-		this.main.appendChild(canvas1);
-		const canvas2 = document.createElement('canvas');
-		this.main.appendChild(canvas2);
-		this.cs2Ctx = canvas2.getContext("2d");
-		this.cs1Ctx = canvas1.getContext("2d");
+		this.createElement(config);
+		this.addStyle();
+		const image = new Image();
+		image.src = config.src;
+		image.onload = () => {
+			this.cs1Ctx = this.appendChild(this.canvas1, image);
+			this.cs2Ctx = this.appendChild(this.canvas2, image);
+			this.cs1Ctx.drawImage(image, 0, 0);
+			this.cs2Ctx.fillStyle = 'rgba(255, 255, 255, 0)'
+			this.handle();
+		}
+	}
+
+	// 创建标签
+	createElement(config) {
+		this.main = document.querySelector(config.el);
+		this.canvas1 = document.createElement('canvas');
+		this.canvas2 = document.createElement('canvas');
+	}
+
+	// 添加样式
+	addStyle() {
+		this.main.style.position = 'relative';
+		this.canvas1.setAttribute("style", "position: absolute; z-index: 997");
+		this.canvas2.setAttribute("style", "position: absolute; z-index: 998");
+	}
+
+	// 创建
+	appendChild(canvas, image) {
+		canvas.width = image.width;
+		canvas.height = image.height;
+		this.main.appendChild(canvas);
+		return canvas.getContext("2d");
 	}
 	// 切换工具
 	changeTools(tname) {
 		if (tname == 'eraser') {
 			this.isEraser = true;
 			this.lineWidth = 30;
-			$(canvas2).css({ 'cursor': "url('./img/b.jpg'),default" }); // 设置橡皮擦自定义鼠标样式
+			this.canvas2.style.cursor = "url('../img/favicon.ico'),crosshair";// 设置橡皮擦自定义鼠标样式
 		} else if (tname == 'pen') {
 			this.isEraser = false;
 			this.lineWidth = 30;
-			$(canvas2).css({ 'cursor': 'crosshair' }); // 设置画笔系统鼠标样式(十字)
+			this.canvas2.style.cursor = "crosshair";// 设置橡皮擦自定义鼠标样式
+		} else if(tname == 'save') {
+			this.downloadImg();
 		}
 	}
 	handle() {
-		this.canvas2.onmousedown = function (eva) {
+		this.canvas2.onmousedown = (eva) => {
 			var eva = eva || window.event;
 			this.cs2Ctx.lineCap = "round";
 			this.cs2Ctx.lineJoin = "round";
@@ -44,15 +76,14 @@ class AiLabel {
 			this.cs2Ctx.beginPath();
 			this.cs2Ctx.moveTo(x, y);
 			this.flag = true;
-			this.cs2Ctx.lineWidth = lineWidth;
-			this.cs2Ctx.strokeStyle = "#FF0000";
+			this.cs2Ctx.lineWidth = this.lineWidth;
+			this.cs2Ctx.strokeStyle = this.theColor;
 		}
 		// onmousemove事件
 		this.canvas2.onmousemove = (eva) => {
 			if (this.flag) {
 				if (this.isEraser) {
 					var w = this.lineWidth;
-					// noinspection JSAnnotator
 					let pxs = this.cs1Ctx.getImageData(eva.offsetX - w / 2, eva.offsetY - w / 2, w, w);
 					this.cs2Ctx.putImageData(pxs, eva.offsetX - w / 2, eva.offsetY - w / 2);
 				} else {
@@ -65,28 +96,21 @@ class AiLabel {
 			}
 		}
 		// onmouseup事件
-		this.canvas2.onmouseup = function () {
+		this.canvas2.onmouseup = () => {
 			this.flag = false;
 			this.cs2Ctx.closePath();
 		}
 		// onmouseleave事件
-		this.canvas2.onmouseleave = function () {
+		this.canvas2.onmouseleave = () => {
 			this.flag = false;
 			this.cs2Ctx.closePath();
 		}
 	}
+
 	downloadImg() {
-		// var canvas = document.getElementById('canvas2');
-		console.log(this.canvas2);
 		var imgSrc = this.canvas2.toDataURL("image/png");
 		console.log(imgSrc);
 	}
 }
-
-// onmousedown事件
-
-
-// --------- 画笔和橡皮擦功能实现 -------------
-
-// 保存画布
+export default new AiLabel();
 
