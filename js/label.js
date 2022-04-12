9015e4463f6b99609341cf3c081b64de4ea319ed
Your Name <Your email>
2022-04-12 23:16:54 +0800

    svg绘制补充

--- js/label.js
+++ js/label.js
@@ -1,62 +1,93 @@
+import newAiLabel from "./newLabel.js";
 class AiLabel {
-	main;
-	canvasMain;
-	canvas1;
-	canvas2;
+	main; // 祖父级别
+	canvasMain; // cnavas 主体
+	canvas1; // 底图 cnavas1
+	canvas2; // 绘制层 cnavas2
 	cs2Ctx;
 	cs1Ctx;
-	flag = false;  // 记录鼠标按下和抬起事件，标记工具是否开始工作
+	flag = false; // 记录鼠标按下和抬起事件，标记工具是否开始工作
 	isEraser; // 判断工具是画笔还是橡皮擦
+	getMarkerData = []; // 涂抹坐标
+	getRectData = []; // 矩形数据
 	lineWidth = 30; // 设置工具粗细
 	defaultLineWidth = 30;
 	theColor = "#FF0000"; // 设置画笔颜色
 	scale = 1;
+	mode; // 选择样式
 	imgInof = {
 		width: 0,
 		height: 0
-	}
+	};
+	keyCode; // 键盘Code
+	rect_tag = null;
+	// rectData = {
+	// 	startX: 0,
+	// 	startY: 0,
+	// 	endX: 0,
+	// 	endY: 0,
+	// 	layerIndex: 0,
+	// 	layerName: "",
+	// 	startState: false
+	// };
+   
 
+	// 设置缩放比例
 	setScale() {
-		this.canvas1.style.transform = `scale(${this.scale})`;
-		this.canvas2.style.transform = `scale(${this.scale})`;
-	}
+		// this.canvas1.style.transform = `scale(${this.scale})`;
+		// this.canvas2.style.transform = `scale(${this.scale})`;
+		this.canvasMain.style.transform = `scale(${this.scale})`;
+	};
 
 	setDraw(theColor) {
 		this.cs2Ctx.strokeStyle = theColor;
-	}
-	/**
-	 * el // 节点id
-	 * src // 图片路径
-	 * @param {*} config
-	 */
+	};
+
 	initMap(config) {
 		this.createElement(config);
-		this.addStyle(config);
 		const image = new Image();
 		image.src = config.src;
 		image.onload = () => {
-			this.imgInof = image
+			this.imgInof = image;
+			this.addStyle(config);
 			this.cs1Ctx = this.appendChild(this.canvas1, image);
-			this.cs2Ctx = this.appendChild(this.canvas2, image);
-			this.handle();
+			this.mode = "rect";
+			switch (this.mode) {
+				case "rect":
+					const div = document.createElement("div");
+					div.setAttribute("style", "position: absolute; z-index: 999; background-color: transparent;");
+					div.setAttribute("id", "svg_body");
+					this.canvasMain.appendChild(div);
+					newAiLabel.createRectLabel("svg_body");
+					break;
+				default:
+					this.cs2Ctx = this.appendChild(this.canvas2, image);
+					break;
+			}
+			this.dragHandler();
+			this.main.onmousewheel = (e) => {
+				this.onMouseScroll(e);
+				this.reactiveLineWidth();
+			}
 		}
-	}
-
+	};
+ 
 	// 创建标签
 	createElement(config) {
 		this.main = document.querySelector(config.el);
 		this.canvasMain = document.createElement('div');
 		this.canvas1 = document.createElement('canvas');
 		this.canvas2 = document.createElement('canvas');
-	}
+	};
 
 	// 添加样式
 	addStyle(config) {
-		this.main.setAttribute("style", `position: absolute; width: 100%; height: 100%;display: flex;justify-content: center;align-items: center;`);
-		this.canvasMain.setAttribute("style", `position: relative;`)
-		this.canvas1.setAttribute("style", `position: absolute; z-index: 997; background-image: url('${config.src}');transition-delay: 0.8;`);
-		this.canvas2.setAttribute("style", "position: absolute; z-index: 999; background-color: transparent;transition-delay: 0.8;");
-	}
+		this.main.setAttribute("style", `position: absolute; width: 100%; height: 100%;`);
+		this.canvasMain.setAttribute("style", `position: absolute; width: ${this.imgInof.width}px; height: ${this.imgInof.height}px; `);
+		this.canvasMain.setAttribute("id","qs_main");
+		this.canvas1.setAttribute("style", `position: absolute; z-index: 997; background-image: url('${config.src}');transition: all 0.1s;`);
+		this.canvas2.setAttribute("style", "position: absolute; z-index: 999; background-color: transparent;transition: all 0.1s;");
+	};
 
 	// 创建
 	appendChild(canvas, image) {
@@ -65,88 +96,184 @@ class AiLabel {
 		canvas.height = image.height;
 		this.canvasMain.appendChild(canvas);
 		return canvas.getContext("2d");
-	}
+	};
 
 	reactiveLineWidth() {
 		this.lineWidth = this.defaultLineWidth / this.scale;
-	}
+	};
 
 	// 切换工具
 	changeTools(tname) {
-		if (tname == 'eraser') {
-			this.isEraser = true;
-			this.reactiveLineWidth()
-			this.canvas2.style.cursor = "url('../img/favicon.ico'),crosshair";// 设置橡皮擦自定义鼠标样式
-		} else if (tname == 'pen') {
-			this.isEraser = false;
-			this.reactiveLineWidth()
-			this.canvas2.style.cursor = "crosshair";// 设置橡皮擦自定义鼠标样式
-		} else if (tname == 'save') {
-			this.downloadImg();
-		}
-	}
+		this.mode = tname;
+		console.log(tname);
+		switch (tname) {
+			case "eraser":
+				this.isEraser = true;
+				// this.reactiveLineWidth();
+				// this.canvas2.style.cursor = "url('../img/favicon.ico'),crosshair"; // 设置橡皮擦自定义鼠标样式
+				// this.drawMasker();
+				break;
+			case "pen":
+				this.isEraser = false;
+				// this.reactiveLineWidth()
+				// this.canvas2.style.cursor = "crosshair"; // 设置橡皮擦自定义鼠标样式
+				// this.drawMasker();
+				break;
+			case "save":
+				this.downloadImg();
+				break;
+			case "rect":
+				// this.canvas2.style.cursor = "crosshair";
+				// this.cs2Ctx.lineWidth = 2 / this.scale;
+				// this.cs2Ctx.strokeStyle = "red";
+				// this.drawRectMain();
+
+
+				newAiLabel.drawRectMain();
+				break;
+		};
+	};
+
 	onMouseScroll(e) {
 		e.preventDefault();
 		const wheel = e.wheelDelta || -e.detail;
 		const delta = Math.max(-1, Math.min(1, wheel));
-		if (delta < 0) {//向下滚动
+		if (delta < 0) { //向下滚动
 			if (this.scale < 0.5) {
 				this.scale = this.scale;
 			} else {
 				this.scale -= 0.1;
 				this.setScale();
 			}
-		} else {//向上滚动
+		} else { // 向上滚动
 			this.scale += 0.1;
 			this.setScale();
 		}
-	}
-
-	handle() {
-		this.main.onmousewheel = (e) => {
-			this.onMouseScroll(e)
-			this.reactiveLineWidth()
+	};
+	drawRect() {
+		const { startState } = this.rectData;
+		if (startState) {
+			this.cs2Ctx.clearRect(0, 0, this.canvas2.width, this.canvas2.height);
+			const ract = {
+				x: this.rectData.startX,
+				y: this.rectData.startY,
+				width: this.rectData.endX - this.rectData.startX,
+				height: this.rectData.endY - this.rectData.startY
+			}
+			this.getRectData.push(ract);
+			this.rectData.startState = false;
+			this.continuousDrawRact();
+			this.cs2Ctx.closePath();
 		}
+	};
+	// 绘制矩形
+	drawRectMain() {
 		this.canvas2.onmousedown = (event) => {
+			this.rectData.startX = event.offsetX;
+			this.rectData.startY = event.offsetY;
+			this.rectData.startState = true;
+			this.canvas2.onmousemove = (event) => {
+				const { startState } = this.rectData;
+				if (startState) {
+					this.cs2Ctx.clearRect(0, 0, this.canvas2.width, this.canvas2.height);
+					this.rectData.endX = event.offsetX;
+					this.rectData.endY = event.offsetY;
+					this.cs2Ctx.beginPath();
+					this.cs2Ctx.moveTo(event.offsetX, event.offsetY);
+					this.cs2Ctx.strokeRect(this.rectData.startX, this.rectData.startY, this.rectData.endX - this.rectData.startX, this.rectData.endY - this.rectData.startY);
+					this.continuousDrawRact();
+				}
+			}
+		}
+		this.canvas2.onmouseup = () => { this.drawRect(); };
+		// onmouseleave事件
+		this.canvas2.onmouseleave = () => { this.drawRect(); }
+	};
+	// 连续绘制
+	continuousDrawRact() {
+		for (let item of this.getRectData) {
+			this.cs2Ctx.strokeRect(item.x, item.y, item.width, item.height);
+		}
+	};
+
+	// 涂抹橡皮擦
+	drawMasker() {
+		this.canvas2.onmousedown = (event) => {
+			if (this.keyCode === "keyCode") {
+				return
+			}
+			var x = event.offsetX;
+			var y = event.offsetY;
 			var event = event || window.event;
 			this.cs2Ctx.lineCap = "round";
 			this.cs2Ctx.lineJoin = "round";
-			var x = event.offsetX;
-			var y = event.offsetY;
 			this.cs2Ctx.beginPath();
 			this.cs2Ctx.moveTo(x, y);
 			this.flag = true;
 			this.cs2Ctx.lineWidth = this.defaultLineWidth / this.scale;
 			this.cs2Ctx.strokeStyle = this.theColor;
-		}
-		// onmousemove事件
-		this.canvas2.onmousemove = (event) => {
-			if (this.flag) {
-				if (this.isEraser) {
-					var w = this.lineWidth;
-					let pxs = this.cs1Ctx.getImageData(event.offsetX - w / 2, event.offsetY - w / 2, w, w);
-					this.cs2Ctx.putImageData(pxs, event.offsetX - w / 2, event.offsetY - w / 2);
-				} else {
-					var event = event || window.event;
-					var x = event.offsetX;
-					var y = event.offsetY;
-					this.cs2Ctx.lineTo(x, y);
-					this.cs2Ctx.stroke();
+			// onmousemove事件
+			this.canvas2.onmousemove = (event) => {
+				if (this.flag) {
+					if (this.isEraser) {
+						var w = this.lineWidth;
+						let pxs = this.cs1Ctx.getImageData(event.offsetX - w / 2, event.offsetY - w / 2, w, w);
+						this.cs2Ctx.putImageData(pxs, event.offsetX - w / 2, event.offsetY - w / 2);
+					} else {
+						var event = event || window.event;
+						var x = event.offsetX;
+						var y = event.offsetY;
+						this.cs2Ctx.lineTo(x, y);
+						this.cs2Ctx.stroke();
+						this.getMarkerData.push({ x: x, y: y });
+					}
 				}
-			}
-		}
-		// onmouseup事件
+			};
+		};
+
 		this.canvas2.onmouseup = () => {
 			this.flag = false;
 			this.cs2Ctx.closePath();
-		}
+		};
 		// onmouseleave事件
 		this.canvas2.onmouseleave = () => {
 			this.flag = false;
 			this.cs2Ctx.closePath();
 		}
+	};
 
+	// 拖动
+	dragHandler() {
+		document.onkeydown = (e) => {
+			if (e.code === "Space") {
+				this.canvas2.style.cursor = "move";
+				this.keyCode = "keyCode";
+				this.dragEvent();
+			}
+		}
+		document.onkeyup = () => {
+			this.keyCode = null;
+			this.canvas2.style.cursor = "crosshair";
+		}
+	};
+	// 多动
+	dragEvent() {
+		const box = this.canvasMain;
+		box.onmousedown = (el) => {
+			document.onmousemove = (e) => {
+				if (!this.keyCode) {
+					return
+				}
+				box.style.left = e.clientX - el.offsetX + "px";
+				box.style.top = e.clientY - el.offsetY + "px";
+				document.onmouseup = () => {
+					document.onmousemove = null;
+					document.onmouseup = null;
+				};
+			};
+		};
 	}
+
 	downloadImg() {
 		const imgSrc = this.canvas2.toDataURL("image/png");
 		console.log(imgSrc);
---
+++ js/newLabel.js
@@ -0,0 +1,279 @@
+class AiLabel {
+    startState = false; // 开始标注状态
+    labelData = [{
+        id: new Date().getTime(),
+        width: 220,
+        height: 220,
+        x: 220,
+        y: 40,
+        style: {
+            fillStyle: 'rgb(239, 239, 239)',
+            stroke: '#2c9c21',
+            strokeWidth: 1
+        }
+    }, {
+        id: new Date().getTime() + 1,
+        width: 220,
+        height: 220,
+        x: 100,
+        y: 100,
+        style: {
+            fillStyle: 'rgb(239, 239, 239)',
+            stroke: '#2c9c21',
+            strokeWidth: 1
+        }
+    }];
+    svg_body = null;
+    svg = null;
+    // 字符串转Dom
+    dom = "<select><option value='标签名称1'>标签名称1</option><option value='saab'>Saab</option><option value='opel'>Opel</option><option value='audi'>Audi</option></select>";
+    virtualNode = null; // 虚拟节点
+    // 存放绘制时的数据
+    drawWhenData = {
+        start_x: 0,
+        start_y: 0,
+        width: 0,
+        height: 0
+    }
+    
+    // 初始化
+    createRectLabel(config) {
+        this.svg = this.createTag("svg", {
+            version: "1.1",
+            xmlns: 'http://www.w3.org/2000/svg',
+            baseProfile: "full",
+            width: "100%",
+            height: "100%"
+        })
+        console.log(config);
+        this.svg_body = document.querySelector(`#${config}`);
+        this.svg_body.appendChild(this.svg);
+
+        // this.createElement(this.labelData);
+    }
+
+    createNode(dom) {
+        const template = dom;
+        let tempNode = document.createElement('div');
+        tempNode.innerHTML = template;
+        return tempNode.firstChild;
+    }
+
+
+    // 创建标签
+    createTag(tag, objAttr) {
+        var svgNS = 'http://www.w3.org/2000/svg';
+        var oTag = document.createElementNS(svgNS, tag);
+        for (let attr in objAttr) {
+            oTag.setAttribute(attr, objAttr[attr]);
+        }
+        return oTag;
+    };
+
+    // 清除编辑节点
+    clearEditNode() {
+        const node = document.querySelectorAll(`.edit`);
+        if (node.length) {
+            for (let i = 0; i < node.length; i++) {
+                node[i].style.display = "none"
+            }
+        }
+    };
+
+    // 展示编辑节点
+    showEditNode(id) {
+        
+        const node = document.querySelectorAll(`#${id} .edit`);
+        for (let i = 0; i < node.length; i++) {
+            node[i].style.display = "block";
+        }
+    };
+
+    // 创矩形
+    createRect(x, y, h, w) {
+        return {
+            width: w,
+            height: h,
+            x: x,
+            y: y,
+            style: `fill:rgb(239, 239, 239);fill-opacity: 0.1; stroke: #2c9c21; stroke-width: 1;`
+        }
+    };
+
+
+    createEditNode(g, w, h, x, y) {
+        // 左上
+        // const rect_l_t = createTag("rect", { class: 'edit rect_l_t', width: '8', height: '8', fill: 'red', x: x - 4, y: y - 4, style: "cursor: nw-resize;" });
+        // 左中
+        // const rect_l_c = createTag("rect", { class: 'edit rect_l_c', width: '8', height: '8', fill: 'red', x: x - 4, y: y - 4 + (h / 2), style: "cursor: ew-resize;" });
+        // 左下
+        // const rect_l_b = createTag("rect", { class: 'edit rect_l_b', width: '8', height: '8', fill: 'red', x: x - 4, y: y - 4 + h, style: "cursor: sw-resize;" });
+        // 上中
+        // const rect_t_c = createTag("rect", { class: 'edit rect_t_c', width: '8', height: '8', fill: 'red', x: x - 4 + (w / 2), y: y - 4, style: "cursor: n-resize" });
+        // 右上
+        const rect_r_t = this.createTag("rect", {
+            class: 'edit rect_r_t',
+            width: '8',
+            height: '8',
+            fill: 'red',
+            x: x - 4 + w,
+            y: y - 4,
+            style: "cursor: ne-resize;"
+        });
+        // 下中
+        // const rect_b_c = createTag("rect", { class: 'edit rect_b_c', width: '8', height: '8', fill: 'red', x: x - 4 + (w / 2), y: y - 4 + h, style: "cursor: s-resize;" });
+        // 右下
+        const rect_r_b = this.createTag("rect", {
+            class: 'edit rect_r_b',
+            width: '8',
+            height: '8',
+            fill: 'red',
+            x: x - 4 + w,
+            y: y - 4 + h,
+            style: "cursor: se-resize;"
+        });
+        // 右中
+        // const rect_r_c = createTag("rect", { class: 'edit rect_r_c', width: '8', height: '8', fill: 'red', x: x - 4 + w, y: y - 4 + (h / 2), style: "cursor: ew-resize;" });
+        // g.appendChild(rect_l_t);
+        // g.appendChild(rect_t_c);
+        g.appendChild(rect_r_t);
+
+        // g.appendChild(rect_l_c);
+        // g.appendChild(rect_l_b);
+        // g.appendChild(rect_b_c);
+        g.appendChild(rect_r_b);
+        const foreignObject = this.createTag("foreignObject", {
+            class: `foreignObject`,
+            height: 22,
+            width: w - 8,
+            x,
+            y: y - 4 + h,
+        });
+        foreignObject.appendChild(this.createNode(this.dom));
+        g.appendChild(foreignObject);
+        g.addEventListener("dblclick", ()=>{
+            this.handlerG(g)
+        });
+    };
+    // 创建节点
+    createNewNode(id, w, h, x, y) {
+        const g = this.createTag("svg", {
+            id: `qs_${id}`,
+            width: "100%",
+            height: "100%"
+        });
+        const rect = this.createTag("rect", this.createRect(x, y, h, w));
+        rect.setAttribute("class", "rect");
+        this.svg.appendChild(g);
+        const virtualNode = this.createTag("rect", this.createRect(0, 0, 0, 0));
+        virtualNode.setAttribute("div", "virtualNode");
+        g.appendChild(rect);
+        g.appendChild(virtualNode);
+        this.createEditNode(g, w, h, x, y);
+    };
+	// 绘制矩形
+	drawRectMain() {
+		this.svg_body.onmousedown = (event) => {
+            console.log(event);
+            this.drawWhenData.start_x =  event.offsetX;
+            this.drawWhenData.start_y =  event.offsetY;
+			this.startState = true;
+			this.svg_body.onmousemove = (event) => {
+				if (this.startState) {
+					
+				}
+			}
+		}
+		this.svg_body.onmouseup = () => {  };
+		// onmouseleave事件
+		this.svg_body.onmouseleave = () => {  }
+	};
+    // 设置属性
+    updateAttribute(tag, objAttr) {
+        for (let attr in objAttr) {
+            tag.setAttribute(attr, objAttr[attr]);
+        }
+    }
+
+    // 更新Dom 坐标
+    updateDom(id, e, data) {
+        const {
+            width,
+            height,
+            x,
+            y
+        } = data;
+        const ract = document.querySelector(`#${id} .rect`);
+        // 判断 当前所方式是否小于
+        if (width + (e.clientX - (width + x)) < width / 2 || height + (e.clientY - (height + y)) < height / 2) {
+            return
+        };
+
+        this.updateAttribute(ract, {
+            x,
+            y,
+            width: width + (e.clientX - (width + x)),
+            height: height + (e.clientY - (height + y))
+        });
+        const rect_r_t = document.querySelector(`#${id} .rect_r_t`);
+        this.updateAttribute(rect_r_t, {
+            x: width + (e.clientX - width) - 4,
+            y: y - 4
+        });
+
+        const rect_r_b = document.querySelector(`#${id} .rect_r_b`);
+        this.updateAttribute(rect_r_b, {
+            x: width + (e.clientX - width) - 4,
+            y: height + (e.clientY - height) - 4
+        });
+
+        const foreignObject = document.querySelector(`#${id} .foreignObject`);
+        this.updateAttribute(foreignObject, {
+            x,
+            y: height + (e.clientY - height) - 4,
+            height: 22,
+            width: width + (e.clientX - (width + x)) - 8,
+        });
+    }
+
+    // 双击编辑图片
+    handlerG(tag) {
+        console.log(tag);
+        const allData = this.labelData.filter(item => `qs_${item.id}` === tag.getAttribute('id'));
+        tag.parentNode.removeChild(tag);
+        this.svg.appendChild(tag);
+        if (allData.length) {
+            this.clearEditNode();
+            this.showEditNode(`qs_${allData[0].id}`);
+            this.bindEvent(`qs_${allData[0].id}`, allData[0]);
+        }
+    };
+
+    // 编辑事件绑定
+    bindEvent(id, data) {
+        const dom = document.querySelector(`#${id} .rect_r_b`);
+        let flag = false;
+        // 根据数据获取
+        dom.onmousedown = (event) => {
+            flag = true;
+            this.svg_body.onmousemove = (e) => {
+                if (flag) {
+                    this.updateDom(id, e, data);
+                }
+            }
+            this.svg_body.onmouseup = () => {
+                flag = false;
+            }
+        }
+    };
+    // 创建虚拟svg DOM  id, w, h, x, y
+    createElement(data) {
+        for (let item of data) {
+            this.createNewNode(item.id, item.width, item.height, item.x, item.y)
+            this.clearEditNode();
+        }
+    };
+}
+
+
+export default new AiLabel();
